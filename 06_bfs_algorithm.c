b1e9aba8f0b0358a972eb9185a20ffa2c2443bd0
bikash mahanandia <bikashmahanandia07@gmail.com>
2026-04-23 22:01:14 +0530

    bfs added

--- 06_bfs_algorithm.c
+++ 06_bfs_algorithm.c
@@ -1,1 +1,55 @@
-Nahi likha huun : tu likha hai toh share kar..
+#include <stdio.h>
+
+#define V 5
+#define MAXQ 100
+
+// BFS for single connected component
+void bfs(int adj[V][V], int res[V], int *resSize) {
+    int visited[V] = {0};
+    int q[MAXQ];
+    int front = 0, rear = 0;
+    int src = 0;
+    visited[src] = 1;
+    q[rear++] = src;
+
+    while (front < rear) {
+        int curr = q[front++];
+        res[(*resSize)++] = curr;
+
+        // visit all the unvisited
+        // neighbours of current node
+        for (int x = 0; x < V; x++) {
+            if (adj[curr][x] && !visited[x]) {
+                visited[x] = 1;
+                q[rear++] = x;
+            }
+        }
+    }
+}
+
+void addEdge(int adj[V][V], int u, int v) {
+    adj[u][v] = 1;
+    adj[v][u] = 1;  // undirected
+}
+
+int main() {
+    int adj[V][V] = {0};
+
+    
+    // creating adjacency list
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 0);
+    addEdge(adj, 2, 0);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 2, 4);
+
+    int res[V];
+    int resSize = 0;
+
+    bfs(adj, res, &resSize);
+
+    for (int i = 0; i < resSize; i++)
+        printf("%d ", res[i]);
+
+    return 0;
+}
--- 07_dfs_algorithm.c
+++ 07_dfs_algorithm.c
@@ -1,25 +1,39 @@
 #include<stdio.h>
 #define max 5
-    int adj[max][max]={{0,1,1,0,0},
-                       {1,0,0,1,1},
-                       {1,0,0,0,0},
-                       {0,1,0,0,0},
-                       {0,1,0,0,0}};
-        int visit[max]={0};
 
-void dfs(int node){
-    printf("%d->",node);
-    visit[node]=1;
-    for(int i=0; i<max; i++){
-        if(adj[node][i]==1 && visit[i]==0){
-            dfs(i);
+int adj[max][max] = {
+    {0,1,1,0,0},
+    {1,0,0,1,1},
+    {1,0,0,0,0},
+    {0,1,0,0,0},
+    {0,1,0,0,0}
+};
+
+int visit[max] = {0};
+int queue[max];
+
+void bfs(int node){
+    int front = 0, rear = 0;
+
+    printf("%d->", node);
+    visit[node] = 1;
+    queue[rear] = node;
+
+    while(front <= rear){
+        node = queue[front++];
+
+        for(int i = 0; i < max; i++){
+            if(adj[node][i] == 1 && visit[i] == 0){
+                printf("%d->", i);
+                visit[i] = 1;
+                queue[++rear] = i;
+            }
         }
     }
 }
 
-int main()
-{
-    printf("DFS traversal : ");
-    dfs(0);
+int main(){
+    printf("BFS traversal : ");
+    bfs(0);
     return 0;
 }
