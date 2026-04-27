74e6bb21daf33b5663be08db4d62396fc57e71a4
bikash mahanandia <bikashmahanandia07@gmail.com>
2026-04-27 12:16:46 +0530

    This is the bfs-updated

--- 06_bfs_algorithm.c
+++ 06_bfs_algorithm.c
@@ -1,55 +1,48 @@
-#include <stdio.h>
+#include<stdio.h>
+#define max 5
 
-#define V 5
-#define MAXQ 100
+int adj[max][max] = {
+    {0,1,1,0,0},
+    {1,0,0,1,1},
+    {1,0,0,0,0},
+    {0,1,0,0,0},
+    {0,1,0,0,0}
+};
 
-// BFS for single connected component
-void bfs(int adj[V][V], int res[V], int *resSize) {
-    int visited[V] = {0};
-    int q[MAXQ];
-    int front = 0, rear = 0;
-    int src = 0;
-    visited[src] = 1;
-    q[rear++] = src;
+int visit[max] = {0};
+int queue[max];
+int front = -1, rear = -1;
 
-    while (front < rear) {
-        int curr = q[front++];
-        res[(*resSize)++] = curr;
+void enqueue(int node){
+    if(rear == max-1) return;
+    if(front == -1) front = 0;
+    queue[++rear] = node;
+}
 
-        // visit all the unvisited
-        // neighbours of current node
-        for (int x = 0; x < V; x++) {
-            if (adj[curr][x] && !visited[x]) {
-                visited[x] = 1;
-                q[rear++] = x;
+int dequeue(){
+    if(front == -1 || front > rear) return -1;
+    return queue[front++];
+}
+
+void bfs(int start){
+    enqueue(start);
+    visit[start] = 1;
+
+    while(front <= rear){
+        int node = dequeue();
+        printf("%d->", node);
+
+        for(int i = 0; i < max; i++){
+            if(adj[node][i] == 1 && visit[i] == 0){
+                enqueue(i);
+                visit[i] = 1;
             }
         }
     }
 }
 
-void addEdge(int adj[V][V], int u, int v) {
-    adj[u][v] = 1;
-    adj[v][u] = 1;  // undirected
-}
-
-int main() {
-    int adj[V][V] = {0};
-
-    
-    // creating adjacency list
-    addEdge(adj, 1, 2);
-    addEdge(adj, 1, 0);
-    addEdge(adj, 2, 0);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 2, 4);
-
-    int res[V];
-    int resSize = 0;
-
-    bfs(adj, res, &resSize);
-
-    for (int i = 0; i < resSize; i++)
-        printf("%d ", res[i]);
-
+int main(){
+    printf("BFS traversal : ");
+    bfs(0);
     return 0;
 }
